6da25d2da152a782493c6dbc551a74dae0ae7952
feefles <eyeung@seas.upenn.edu>
2014-10-07 22:16:58 -0400

    i think heroku was configured wrong

--- togethr/views.py
+++ togethr/views.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, request, escape, session
 
 #main display
 
-@app.route("/hi")
+@app.route("/")
 def display():
 	if 'username' in session:
 		return render_template('main.html', user=session['username'])
